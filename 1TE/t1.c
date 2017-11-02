c9ceac9cc88a97b88edd4d4eee91c21785beccdf
ogoes17 <otaviosilva820@gmail.com>
2017-11-02 20:16:13 -0200

    Remove

--- 1TE/t1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void main(void){
-	
-	printf("%i\n", strcmp("1", "5"));
-
-}
--- TEstEstuDO/t.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "ord.h"
-
-void main(){
-	printf("\a");
-}
