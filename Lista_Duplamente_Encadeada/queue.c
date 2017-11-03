93938a4c3e69cf06fc8589960e25075a07536160
Otávio Goes <otaviosilva820@gmail.com>
2017-11-03 09:51:25 -0200

    for

--- Lista_Duplamente_Encadeada/queue.c
+++ Lista_Duplamente_Encadeada/queue.c
@@ -11,18 +11,11 @@ void main(){
 	Queue* Q = Queue_Criar();
 	int a;
 	int *A = &a;
-	Queue_Push(Q, 1, 0);
-	Queue_Imprimir(Q, print);
-	Queue_Push(Q, 2, 0);
-	Queue_Imprimir(Q, print);
-	Queue_Push(Q, 3, 0);
-	Queue_Imprimir(Q, print);
-	Queue_Push(Q, 4, 0);
-	Queue_Imprimir(Q, print);
-	Queue_Push(Q, 5, 0);
-	Queue_Imprimir(Q, print);
-	Queue_Push(Q, 6, 0);
-	Queue_Imprimir(Q, print);
+	
+	for(int i = 0; i < 6; ++i){
+		Queue_Push(Q, i+1, 0);
+		Queue_Imprimir(Q, print);
+	}
 	A = Queue_Pop1(Q, 2);
 	Queue_Imprimir(Q, print);
 
